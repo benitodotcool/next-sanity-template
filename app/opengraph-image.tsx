5856a4bef559b818b6f4048524caeb02185171cb
Valentin Benito Bousquet <bousquet.valentin@gmail.com>
2023-05-30 22:38:37 +0200

    add robot.ts + og-image + rename _reset.css

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from "next/server"
+import { groq } from "next-sanity"
+import { clientFetch } from "@/lib/sanity"
+
+export const size = { width: 1200, height: 630 }
+export const contentType = 'image/png'
+export const runtime = 'edge'
+
+export default async function og() {
+  const metaTitle = await clientFetch<string>(IMAGE_QUERY, {next: {revalidate: 10}})
+
+  console.log(metaTitle)
+
+  return new ImageResponse(
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '1200px',
+        height: '630px'
+      }}
+    >
+      <h1>{metaTitle}</h1>
+    </div>
+  )
+}
+
+const IMAGE_QUERY = groq`
+  *[_type == 'settings'][0].meta.title.it
+`
